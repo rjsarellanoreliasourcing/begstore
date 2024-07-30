b51e997f89c5a4e8da34547ce11cb72538378f89
rjsarellanoreliasourcing <ryanjoseph.a@reliasourcing.com>
2024-07-31 03:57:17 +0800

    commit 07312024

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,8 +2,18 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User } from 'src/schema/user.schema';
+import { CreateUserDto } from './dto/CreateUser.dto';
 
 @Injectable()
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
+
+  createUser(createUser: CreateUserDto) {
+    const newUser = new this.userModel(CreateUserDto);
+    return newUser.save();
+  }
+
+  getUsers() {
+    return this.userModel.find();
+  }
 }
